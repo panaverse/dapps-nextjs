5b35de27f1c98e38eabba065d93dcb1e9f5dbc8c
Zia Khan <ziaukhan@hotmail.com>
2022-08-03 16:03:22 +0500

    add step 09a

---
+++ step09a_read_smart_contracts_nextjs/README.md
@@ -0,0 +1,51 @@
+# Read Smart Contracts using Ethers.js in Browser
+
+[Mint Georli Ethers from this Faucet](https://goerlifaucet.com/)
+
+[Mint Georli DAI from the contract mentioned in the post on Etherscan](https://ethereum.stackexchange.com/questions/72388/does-rinkeby-have-a-faucet-where-i-can-fill-a-wallet-with-dai)
+
+[Georli DAI Contract](https://goerli.etherscan.io/address/0x97cb342Cf2F6EcF48c1285Fb8668f5a4237BF862)
+
+
+npx create-next-app step09a_read_smart_contracts_nextjs --ts
+
+yarn add ethers
+
+
+
+
+
+This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+You can start editing the page by modifying `pages/index.tsx`. The page auto-updates as you edit the file.
+
+[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.ts`.
+
+The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
+
+## Learn More
+
+To learn more about Next.js, take a look at the following resources:
+
+- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
+- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+
+You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+
+## Deploy on Vercel
+
+The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+
+Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
---
+++ step09a_read_smart_contracts_nextjs/pages/index.tsx
@@ -0,0 +1,134 @@
+import type { NextPage } from 'next';
+import { useState, useEffect } from "react";
+import { signMessage } from "../utils/sign";
+import Link from "next/link";
+import Metamask from "../component/Metamask";
+import { ethers } from "ethers";
+
+interface ClientStatus{
+  isConnected: boolean;
+  address?: string;
+  balance?: string;
+}
+
+
+const Index: NextPage = () => {
+
+  const [haveMetamask, sethaveMetamask] = useState<boolean>(true);
+
+  const [clientStatus, setClientStatus] = useState<ClientStatus>({
+    isConnected: false,
+  });
+
+  const ERC20_ABI = [
+    "function name() view returns (string)",
+    "function symbol() view returns (string)",
+    "function totalSupply() view returns (uint256)",
+    "function balanceOf(address) view returns (uint)",
+];
+
+  let provider: ethers.providers.Web3Provider;
+  let contract: ethers.Contract;
+
+  const checkConnection = async () => {
+    const { ethereum } = window as any;
+    if (ethereum) {
+      sethaveMetamask(true);
+      
+      const accounts: string[] = await ethereum.request({ method: "eth_accounts" });
+      if (accounts.length > 0) {
+        setClientStatus({
+          isConnected: true,
+          address: accounts[0],
+        });
+      } else {
+        setClientStatus({
+          isConnected: false,
+        });
+      }
+    } else {
+      sethaveMetamask(false);
+    }
+  };
+
+
+  const connectWeb3 = async () => {
+    //console.log("In ConnectWeb3: Start");
+    try {
+      const { ethereum } = window as any;
+
+      if (!ethereum) {
+        console.log("Metamask not detected");
+        return;
+      }
+      
+      const accounts: string[] = await ethereum.request({method: "eth_requestAccounts"});
+
+      console.log("In ConnectWeb3: After")
+      
+      //await updateBalance(accounts[0]);
+
+    } catch (error) {
+      console.log("Error connecting to metamask", error);
+    }
+  };
+
+  const updateBalance = async(account: string) => {
+    console.log("updateBalance: Begin");
+    provider = new ethers.providers.Web3Provider((window as any).ethereum);
+    const address: string = '0x97cb342Cf2F6EcF48c1285Fb8668f5a4237BF862'; // DAI Contract https://goerli.etherscan.io/address/0x97cb342Cf2F6EcF48c1285Fb8668f5a4237BF862
+    contract = new ethers.Contract(address, ERC20_ABI, provider);
+    const balance: ethers.BigNumber = await contract.balanceOf(account);
+    const balanceFormated: string = ethers.utils.formatEther(contract.balanceOf(account));
+    console.log("updateBalance: Balance: " + balance);
+    setClientStatus({
+      isConnected: true,
+      address: account,
+      balance: balanceFormated
+    });
+
+    console.log("Balance" + clientStatus.balance);
+
+  };
+
+
+  useEffect(() => {
+    checkConnection();
+  }, []);
+
+
+  return (
+    <>
+      <section className="container d-flex">
+        <main>
+          <h1 className="main-title">Awesome DApp</h1>
+
+          <div>
+            {!haveMetamask ? (
+              <Metamask />
+            ) : clientStatus.isConnected ? (
+              <>
+                <br />
+                <h2>You're connected ✅</h2>
+                <div>My Metamask Account:</div>
+                <div>{clientStatus?.address}</div>
+                <div>My DAI Balance:</div>
+                <div>{clientStatus?.balance}</div>
+              </>
+            ) : (
+              <>
+                <br />
+                <button className="btn connect-btn" onClick={connectWeb3}>
+                  Connect Wallet
+                </button>
+              </>
+            )}
+          </div>
+          {/* ---- */}
+        </main>
+      </section>
+    </>
+  );
+};
+
+export default Index;
